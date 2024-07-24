75ac057bd38f71463919b8d3310f26710c048a6b
BiggestBonk <harry.walsh505@gmail.com>
2024-07-24 16:46:13 +1200

    add playgame function

--- script.js
+++ script.js
@@ -40,11 +40,11 @@ function getPlayerChoice() {
     //return choice
 }
 
-
-
+//play game function
+function playGame() {
 let playerScore = 0
 let computerScore = 0
-
+//capitalizes choice
 function capitalize(text) {
 let start = text.substr(0,1)
 let end = text.substr(1)
@@ -52,7 +52,7 @@ start = start.toUpperCase()
 end = end.toLowerCase()
 return start.concat(end)
 }
-
+//play round function
 function playRound(playerChoice, compChoice) {
 //compare choices of player and comp
 //if player and comp choose same choice, tie
@@ -61,24 +61,41 @@ if (playerChoice === compChoice){
 //if player scissors and comp paper, player win
 } else if (playerChoice === "scissors" && compChoice === "paper"){
     console.log("You win, Scissors beats Paper")
+    playerScore += 1
 //if player paper and comp rock, player win
 }else if (playerChoice === "paper" && compChoice === "rock"){
     console.log("You win, Paper beats Rock")
+    playerScore += 1
 //if player rock and comp scissors,player win
 }else if (playerChoice === "rock" && compChoice === "scissors"){
     console.log("You win, Rock beats Scissors")
+    playerScore += 1
 //otherwise, lose
 }else { 
  playerChoice = capitalize(playerChoice)
  compChoice = capitalize(compChoice)   
-    console.log(`You lose, ${compChoice} beats ${playerChoice}`)}
+    console.log(`You lose, ${compChoice} beats ${playerChoice}`)
+    computerScore += 1
+}
 //log winner, store score
 
 }
     
-const playerSelection = getPlayerChoice()
-const computerSelection = getComputerChoice()
+//const playerSelection = getPlayerChoice()
+//const computerSelection = getComputerChoice()
 
-playRound(playerSelection, computerSelection)
+playRound(getPlayerChoice(), getComputerChoice())
+    console.log(`player: ${playerScore} computer: ${computerScore}`)
+playRound(getPlayerChoice(), getComputerChoice())
+    console.log(`player: ${playerScore} computer: ${computerScore}`)
+playRound(getPlayerChoice(), getComputerChoice())
+    console.log(`player: ${playerScore} computer: ${computerScore}`)
+playRound(getPlayerChoice(), getComputerChoice())
+    console.log(`player: ${playerScore} computer: ${computerScore}`)
+playRound(getPlayerChoice(), getComputerChoice())
+    console.log(`player: ${playerScore} computer: ${computerScore}`)   
 
+}
 
+console.log(playGame())
+//end play game here
