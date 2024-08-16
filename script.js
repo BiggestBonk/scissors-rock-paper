ef2fd62866c0bcd43a2ee71b3e7196a81fc0a5bd
BiggestBonk <harry.walsh505@gmail.com>
2024-08-16 17:38:56 +1200

    ruh roh finished project without commiting

--- script.js
+++ script.js
@@ -1,101 +1,103 @@
-function getComputerChoice () {
-    //a number 1 through 3 will be generated.
-    let compChoice = Math.floor(Math.random()* 3) + 1  
-    // if 1 it will equal rock 2 for scissors and 3 for paper
-    if (compChoice === 1){
-        compChoice = "rock"
-        return compChoice
-    }else if (compChoice === 2){
-        compChoice = "scissors"
-        return compChoice
-    }else if (compChoice === 3){
-        compChoice = "paper"
-        return compChoice
-    }
+const choices = ["Rock", "Paper", "Scissors"]
+const allButtons = document.querySelectorAll("button")
+const div = document.querySelector("div")
+const body = document.querySelector("body")
+
+function getComputerChoice() {
+    let randoMath = Math.floor(Math.random() * 3)
+    let compChoice = choices[randoMath]
+    return compChoice
 }
 
-function getPlayerChoice() {
-    //prompt for player to choose
-    let playerChoice = prompt("Paper, Scissors, rock?")
-    playerChoice = playerChoice.toLowerCase()
-    //check whether the choice is valid
-     if (playerChoice === "paper"||
-        playerChoice === "scissors"||
-        playerChoice === "rock"){
-     return playerChoice }
-        while (playerChoice != "paper" ||
-        playerChoice != "scissors"||
-        playerChoice != "rock") {
-        playerChoice = prompt("invalid choice, choose again")
-        playerChoice = playerChoice.toLowerCase()
-        if (playerChoice === "paper"||
-            playerChoice === "scissors"||
-            playerChoice === "rock"){
-         return playerChoice }
-        }
+
+playGame()
 
 
-
-
-    //return choice
-}
-
-//play game function
 function playGame() {
-let playerScore = 0
-let computerScore = 0
-//capitalizes choice
-function capitalize(text) {
-let start = text.substr(0,1)
-let end = text.substr(1)
-start = start.toUpperCase()
-end = end.toLowerCase()
-return start.concat(end)
+    let playerChoice = ""
+    let compChoice = ""
+    let playerScore = 0
+    let compScore = 0
+    const scoreBoard = document.createElement("div")
+    allButtons.forEach(btn => {
+        btn.addEventListener("click", (e) => {
+            playerChoice = e.target.textContent;
+            let result = playRound(playerChoice, compChoice)
+
+            if (result == "win") ++playerScore
+            else if (result == "loss") ++compScore
+            scoreBoard.textContent = `Player Score:${playerScore} Computer Score:${compScore}`
+            body.appendChild(scoreBoard)
+            console.log(playerScore, compScore)
+            if (playerScore + compScore == 5) {
+                disableButtons()
+                const outcome = document.createElement("div")
+                if (playerScore > compScore) {
+                    outcome.textContent = "VICTORY"
+                    body.appendChild(outcome)
+                }
+                else {
+                    outcome.textContent = "DEFEAT"
+                    body.appendChild(outcome)
+                }
+                const restartButton = document.createElement("button")
+                restartButton.textContent = "Another Go?"
+                body.appendChild(restartButton)
+                restartButton.addEventListener("click", () => {
+                    outcome.remove()
+                    playerScore = 0
+                    compScore = 0
+                    enableButtons()
+                    restartButton.remove()
+                    scoreBoard.remove()
+                })
+            }
+        })
+    })
 }
-//play round function
+
+function disableButtons() {
+    const rpsButton = document.querySelectorAll(".rpsButton")
+    rpsButton.forEach(btn => {
+        btn.setAttribute("disabled", "")
+    })
+}
+
+function enableButtons() {
+    const rpsButton = document.querySelectorAll(".rpsButton")
+    rpsButton.forEach(btn => {
+        btn.removeAttribute("disabled")
+    })
+}
+
 function playRound(playerChoice, compChoice) {
-//compare choices of player and comp
-//if player and comp choose same choice, tie
-if (playerChoice === compChoice){
-    console.log("It's a tie")
-//if player scissors and comp paper, player win
-} else if (playerChoice === "scissors" && compChoice === "paper"){
-    console.log("You win, Scissors beats Paper")
-    playerScore += 1
-//if player paper and comp rock, player win
-}else if (playerChoice === "paper" && compChoice === "rock"){
-    console.log("You win, Paper beats Rock")
-    playerScore += 1
-//if player rock and comp scissors,player win
-}else if (playerChoice === "rock" && compChoice === "scissors"){
-    console.log("You win, Rock beats Scissors")
-    playerScore += 1
-//otherwise, lose
-}else { 
- playerChoice = capitalize(playerChoice)
- compChoice = capitalize(compChoice)   
-    console.log(`You lose, ${compChoice} beats ${playerChoice}`)
-    computerScore += 1
-}
-//log winner, store score
-
-}
-    
-//const playerSelection = getPlayerChoice()
-//const computerSelection = getComputerChoice()
-
-playRound(getPlayerChoice(), getComputerChoice())
-    console.log(`player: ${playerScore} computer: ${computerScore}`)
-playRound(getPlayerChoice(), getComputerChoice())
-    console.log(`player: ${playerScore} computer: ${computerScore}`)
-playRound(getPlayerChoice(), getComputerChoice())
-    console.log(`player: ${playerScore} computer: ${computerScore}`)
-playRound(getPlayerChoice(), getComputerChoice())
-    console.log(`player: ${playerScore} computer: ${computerScore}`)
-playRound(getPlayerChoice(), getComputerChoice())
-    console.log(`player: ${playerScore} computer: ${computerScore}`)   
+    compChoice = getComputerChoice()
+    console.log(compChoice, playerChoice)
+    if (playerChoice === compChoice) {
+        div.textContent = "It's a tie"
+        return "tie"
+    }
+    else if ((playerChoice === choices[0]) && (compChoice === choices[2])) {
+        div.textContent = `You win ${playerChoice} beats ${compChoice}`
+        return "win"
+    }
+    else if ((playerChoice === choices[1]) && (compChoice === choices[0])) {
+        div.textContent = `You win ${playerChoice} beats ${compChoice}`
+        return "win"
+    }
+    else if ((playerChoice === choices[2]) && (compChoice === choices[1])) {
+        div.textContent = `you win ${playerChoice} beats ${compChoice}`
+        return "win"
+    }
+    else {
+        div.textContent = `you lose ${compChoice} beats ${playerChoice}`
+        return "loss"
+    }
 
 }
 
-console.log(playGame())
-//end play game here
+//when player clicks an option, round plays
+//compare player choice with comp choice
+//increment winners score, then start another round
+//loop until 5 rounds are played
+//display winner/loser
